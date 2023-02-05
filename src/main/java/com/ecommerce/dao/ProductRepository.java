22146e2f9c04a8a7ae8b1e8acc0a6b83e94b53e4
Prakash Kafle <kafleprakash96@gmail.com>
2023-02-05 02:31:09 -0600

    Added method to call rest-api by category id

--- src/main/java/com/ecommerce/dao/ProductRepository.java
+++ src/main/java/com/ecommerce/dao/ProductRepository.java
@@ -1,11 +1,16 @@
 package com.ecommerce.dao;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.ecommerce.entity.Product;
 
 @CrossOrigin
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+	Page<Product> findByCategoryId(@Param("id") Long id, Pageable pageable);
 
 }
